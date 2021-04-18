abac5a8db6f58fe910e111813b79dea83f1ed2c8
Eric Branlund <ebranlund@fastmail.com>
2021-04-17 22:39:07 -0700

    When working with a purchase of an unknown flavor, include a check for whether the home is involved (can't leak information in that case since home doesn't show the true flavor).  Resolves https://github.com/angband/angband/issues/4727 .

--- src/ui-store.c
+++ src/ui-store.c
@@ -968,14 +968,19 @@ static bool store_purchase(struct store_context *ctx, int item, bool single)
 			/* Limit to the number that can be carried */
 			amt = MIN(amt, inven_carry_num(obj, false));
 
-			/* Fail if there is no room */
-			if ((amt <= 0) || (!object_flavor_is_aware(obj) && pack_is_full())) {
+			/* Fail if there is no room.  Don't leak information about
+			 * unknown flavors for a purchase (getting it from home doesn't
+			 * leak information since it doesn't show the true flavor). */
+			bool flavor_aware = object_flavor_is_aware(obj);
+			if (amt <= 0 || (!flavor_aware && store->sidx != STORE_HOME &&
+								   pack_is_full())) {
 				msg("You cannot carry that many items.");
 				return false;
 			}
 
-			/* Find the number of this item in the inventory */
-			if (!object_flavor_is_aware(obj))
+			/* Find the number of this item in the inventory.  As above,
+			 * avoid leaking information about unknown flavors. */
+			if (!flavor_aware && store->sidx != STORE_HOME)
 				num = 0;
 			else
 				num = find_inven(obj);
