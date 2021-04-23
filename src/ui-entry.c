1dd2665754a7c3afe4ba1c7bdc5e245a6b9aef07
Mike Searle <msearle5@btinternet.com>
2021-04-23 19:50:10 +0100

    Update for compatibility with previous patch

--- src/ui-entry.c
+++ src/ui-entry.c
@@ -842,7 +842,7 @@ void compute_ui_entry_values_for_gear(const struct ui_entry *entry,
 					break;
 
 				case OBJ_PROPERTY_FLAG:
-					if (object_flag_is_known(obj, ind)) {
+					if (object_flag_is_known(p, obj, ind)) {
 						int v = of_has(cache2->f, ind) ? 1 : 0;
 						int a = 0;
 
@@ -868,7 +868,7 @@ void compute_ui_entry_values_for_gear(const struct ui_entry *entry,
 					break;
 
 				case OBJ_PROPERTY_IGNORE:
-					if (object_element_is_known(obj, ind)) {
+					if (object_element_is_known(p, obj, ind)) {
 						int v = (obj->el_info[ind].flags &
 							EL_INFO_IGNORE) ? 1 : 0;
 						int a = 0;
@@ -897,7 +897,7 @@ void compute_ui_entry_values_for_gear(const struct ui_entry *entry,
 				case OBJ_PROPERTY_RESIST:
 				case OBJ_PROPERTY_VULN:
 				case OBJ_PROPERTY_IMM:
-					if (object_element_is_known(obj, ind)) {
+					if (object_element_is_known(p, obj, ind)) {
 						int v = obj->el_info[ind].res_level;
 						int a = 0;
 
