angband/angband

Add a player argument to is_quest() and dungeon_get_next_level().  Remove duplicated declaration of is_quest() from cave.h leaving the declaration in player-quest.h.  Add a Boolean argument to place_random_stairs() and alloc_stairs() to indicate whether this is a quest level or not.  Cache the result of is_quest() in the dun_data structure for cave generation.  Resolves https://github.com/angband/angband/issues/5027 .  Resolves https://github.com/angband/angband/issues/5028.  Both are part of https://github.com/angband/angband/issues/4934 :  places where callers take a struct chunk or struct player but end up calling functions that use the cave or player globals.

src/cmd-cave.c
@@ -43,6 +43,7 @@
 #include "player-attack.h"
 #include "player-calcs.h"
 #include "player-path.h"
+#include "player-quest.h"
 #include "player-timed.h"
 #include "player-util.h"
 #include "project.h"
@@ -68,7 +69,7 @@ void do_cmd_go_up(struct command *cmd)
 		return;
 	}
 	
-	ascend_to = dungeon_get_next_level(player->depth, -1);
+	ascend_to = dungeon_get_next_level(player, player->depth, -1);
 	
 	if (ascend_to == player->depth) {
 		msg("You can't go up from here!");
@@ -95,7 +96,7 @@ void do_cmd_go_up(struct command *cmd)
  */
 void do_cmd_go_down(struct command *cmd)
 {
-	int descend_to = dungeon_get_next_level(player->depth, 1);
+	int descend_to = dungeon_get_next_level(player, player->depth, 1);
 
 	/* Verify stairs */
 	if (!square_isdownstairs(cave, player->grid)) {
@@ -111,8 +112,9 @@ void do_cmd_go_down(struct command *cmd)
 
 	/* Warn a force_descend player if they're going to a quest level */
 	if (OPT(player, birth_force_descend)) {
-		descend_to = dungeon_get_next_level(player->max_depth, 1);
-		if (is_quest(descend_to) &&
+		descend_to = dungeon_get_next_level(player,
+			player->max_depth, 1);
+		if (is_quest(player, descend_to) &&
 			!get_check("Are you sure you want to descend? "))
 			return;
 	}

src/effect-handler-general.c
@@ -40,6 +40,7 @@
 #include "obj-tval.h"
 #include "player-calcs.h"
 #include "player-history.h"
+#include "player-quest.h"
 #include "player-timed.h"
 #include "player-util.h"
 #include "project.h"
@@ -1040,7 +1041,8 @@ bool effect_handler_RECALL(effect_handler_context_t *context)
 	}
 
 	/* No recall from quest levels with force_descend */
-	if (OPT(player, birth_force_descend) && (is_quest(player->depth))) {
+	if (OPT(player, birth_force_descend)
+			&& is_quest(player, player->depth)) {
 		msg("Nothing happens.");
 		return true;
 	}
@@ -1052,9 +1054,9 @@ bool effect_handler_RECALL(effect_handler_context_t *context)
 	}
 
 	/* Warn the player if they're descending to an unrecallable level */
-	target_depth = dungeon_get_next_level(player->max_depth, 1);
-	if (OPT(player, birth_force_descend) && !(player->depth) &&
-			(is_quest(target_depth))) {
+	target_depth = dungeon_get_next_level(player, player->max_depth, 1);
+	if (OPT(player, birth_force_descend) && !(player->depth)
+			&& is_quest(player, target_depth)) {
 		if (!get_check("Are you sure you want to descend? ")) {
 			return false;
 		}
@@ -1102,10 +1104,10 @@ bool effect_handler_DEEP_DESCENT(effect_handler_context_t *context)
 
 	/* Calculate target depth */
 	int target_increment = (4 / z_info->stair_skip) + 1;
-	int target_depth = dungeon_get_next_level(player->max_depth,
-											  target_increment);
+	int target_depth = dungeon_get_next_level(player, player->max_depth,
+		target_increment);
 	for (i = 5; i > 0; i--) {
-		if (is_quest(target_depth)) break;
+		if (is_quest(player, target_depth)) break;
 		if (target_depth >= z_info->max_depth - 1) break;
 
 		target_depth++;
@@ -2667,7 +2669,7 @@ bool effect_handler_TELEPORT_LEVEL(effect_handler_context_t *context)
 {
 	bool up = true;
 	bool down = true;
-	int target_depth = dungeon_get_next_level(player->max_depth, 1);
+	int target_depth = dungeon_get_next_level(player, player->max_depth, 1);
 	struct monster *t_mon = monster_target_monster(context);
 	struct loc decoy = cave_find_decoy(cave);
 
@@ -2715,11 +2717,12 @@ bool effect_handler_TELEPORT_LEVEL(effect_handler_context_t *context)
 		up = false;
 
 	/* No forcing player down to quest levels if they can't leave */
-	if (!up && is_quest(target_depth))
+	if (!up && is_quest(player, target_depth))
 		down = false;
 
 	/* Can't leave quest levels or go down deeper than the dungeon */
-	if (is_quest(player->depth) || (player->depth >= z_info->max_depth - 1))
+	if (is_quest(player, player->depth)
+			|| (player->depth >= z_info->max_depth - 1))
 		down = false;
 
 	/* Determine up/down if not already done */
@@ -2733,16 +2736,19 @@ bool effect_handler_TELEPORT_LEVEL(effect_handler_context_t *context)
 	/* Now actually do the level change */
 	if (up) {
 		msgt(MSG_TPLEVEL, "You rise up through the ceiling.");
-		target_depth = dungeon_get_next_level(player->depth, -1);
+		target_depth = dungeon_get_next_level(player,
+			player->depth, -1);
 		dungeon_change_level(player, target_depth);
 	} else if (down) {
 		msgt(MSG_TPLEVEL, "You sink through the floor.");
 
 		if (OPT(player, birth_force_descend)) {
-			target_depth = dungeon_get_next_level(player->max_depth, 1);
+			target_depth = dungeon_get_next_level(player,
+				player->max_depth, 1);
 			dungeon_change_level(player, target_depth);
 		} else {
-			target_depth = dungeon_get_next_level(player->depth, 1);
+			target_depth = dungeon_get_next_level(player,
+				player->depth, 1);
 			dungeon_change_level(player, target_depth);
 		}
 	} else {

src/trap.c
@@ -24,6 +24,7 @@
 #include "mon-util.h"
 #include "obj-knowledge.h"
 #include "player-attack.h"
+#include "player-quest.h"
 #include "player-timed.h"
 #include "player-util.h"
 #include "trap.h"
@@ -299,7 +300,7 @@ static int pick_trap(struct chunk *c, int feat, int trap_level)
 		/* Check legality of trapdoors. */
 		if (trf_has(kind->flags, TRF_DOWN)) {
 			/* No trap doors on quest levels */
-			if (is_quest(player->depth)) continue;
+			if (is_quest(player, player->depth)) continue;
 
 			/* No trap doors on the deepest level */
 			if (player->depth >= z_info->max_depth - 1)
@@ -556,7 +557,8 @@ extern void hit_trap(struct loc grid, int delayed)
 		/* Some traps drop you a dungeon level */
 		if (trf_has(trap->kind->flags, TRF_DOWN))
 			dungeon_change_level(player,
-								 dungeon_get_next_level(player->depth, 1));
+				dungeon_get_next_level(player,
+				player->depth, 1));
 
 		/* Some traps drop you onto them */
 		if (trf_has(trap->kind->flags, TRF_PIT))

msearle5/xygos

src/cmd-cave.c
@@ -43,6 +43,7 @@
 #include "player-attack.h"
 #include "player-calcs.h"
 #include "player-path.h"
+#include "player-quest.h"
 #include "player-timed.h"
 #include "player-quest.h"
 #include "player-util.h"
@@ -88,7 +89,7 @@ void do_cmd_go_up(struct command *cmd)
 			maze = player->town->climb;
 	}
 
-	ascend_to = dungeon_get_next_level(player->depth, -1);
+	ascend_to = dungeon_get_next_level(player, player->depth, -1);
 
 	/* Exit a quest */
 	if (exit) {
@@ -132,7 +133,7 @@ void do_cmd_go_up(struct command *cmd)
  */
 void do_cmd_go_down(struct command *cmd)
 {
-	int descend_to = dungeon_get_next_level(player->depth, 1);
+	int descend_to = dungeon_get_next_level(player, player->depth, 1);
 	bool entry = (square(cave, player->grid)->feat == FEAT_ENTRY);
 	struct quest *quest = NULL;
 	const char *maze = "You enter a maze of down staircases.";
@@ -152,8 +153,9 @@ void do_cmd_go_down(struct command *cmd)
 
 	/* Warn a force_descend player if they're going to a quest level */
 	if (OPT(player, birth_force_descend)) {
-		descend_to = dungeon_get_next_level(player->max_depth, 1);
-		if (is_blocking_quest(descend_to) &&
+		descend_to = dungeon_get_next_level(player,
+			player->max_depth, 1);
+		if (is_blocking_quest(player, descend_to) &&
 			!get_check("Are you sure you want to descend? "))
 			return;
 	}
@@ -199,7 +201,7 @@ void do_cmd_go_down(struct command *cmd)
 		if (strchr(dname, ' '))
 			*strchr(dname, ' ') = 0;
 		snprintf(buf, sizeof(buf), "Are you sure you want to enter the %s (level %d)? ",
-			dname, dungeon_get_next_level(0, 1));
+			dname, dungeon_get_next_level(player, 0, 1));
 		buf[sizeof(buf)-1] = 0;
 		if (!get_check(buf))
 			return;
@@ -1611,7 +1613,7 @@ void display_feeling(bool obj_only)
 	}
 
 	/* Or on a quest-monster level */
-	if (is_blocking_quest(player->depth)) {
+	if (is_blocking_quest(player, player->depth)) {
 		msg("You feel a terrible presence here!");
 		return;
 	}

src/effect-handler-general.c
@@ -47,6 +47,7 @@
 #include "player-birth.h"
 #include "player-calcs.h"
 #include "player-history.h"
+#include "player-quest.h"
 #include "player-timed.h"
 #include "player-quest.h"
 #include "player-util.h"
@@ -1372,7 +1373,7 @@ bool effect_handler_RECALL(effect_handler_context_t *context)
 	/* No recall in the endgame */
 	if (!player->town) {
 		if (!player->total_winner) {
-			if (is_blocking_quest(player->depth))
+			if (is_blocking_quest(player, player->depth))
 				msg("Nothing happens - something nearby is blocking it.");
 			else
 				msg("Nothing happens - something below you is blocking it.");
@@ -1384,7 +1385,8 @@ bool effect_handler_RECALL(effect_handler_context_t *context)
 	}
 
 	/* No recall from quest levels with force_descend */
-	if (OPT(player, birth_force_descend) && (is_blocking_quest(player->depth))) {
+
+	if (OPT(player, birth_force_descend) && (is_blocking_quest(player, player->depth))) {
 		msg("Nothing happens - something nearby is blocking it.");
 		return true;
 	}
@@ -1396,9 +1398,9 @@ bool effect_handler_RECALL(effect_handler_context_t *context)
 	}
 
 	/* Warn the player if they're descending to an unrecallable level */
-	target_depth = dungeon_get_next_level(player->max_depth, 1);
+	target_depth = dungeon_get_next_level(player, player->max_depth, 1);
 	if (OPT(player, birth_force_descend) && !(player->depth) &&
-			(is_blocking_quest(target_depth))) {
+			(is_blocking_quest(player, target_depth))) {
 		if (!get_check("Are you sure you want to descend? ")) {
 			return false;
 		}
@@ -1448,7 +1450,7 @@ bool effect_handler_DEEP_DESCENT(effect_handler_context_t *context)
 
 	/* Calculate target depth */
 	int target_increment = (4 / z_info->stair_skip) + 1;
-	int target_depth = dungeon_get_next_level(player->depth,
+	int target_depth = dungeon_get_next_level(player, player->depth,
 											  target_increment);
 	int levels = 5;
 
@@ -1457,9 +1459,8 @@ bool effect_handler_DEEP_DESCENT(effect_handler_context_t *context)
 		levels = 1;
 
 	for (i = levels; i > 0; i--) {
-		if (is_blocking_quest(target_depth)) break;
+		if (is_blocking_quest(player, target_depth)) break;
 		if (target_depth >= z_info->max_depth - 1) break;
-
 		target_depth++;
 	}
 
@@ -3403,7 +3404,7 @@ bool effect_handler_TELEPORT_TO(effect_handler_context_t *context)
 
 static bool effect_change_level(effect_handler_context_t *context, const char *rise, const char *sink, bool up, bool down, bool teleport)
 {
-	int target_depth = dungeon_get_next_level(player->max_depth, 1);
+	int target_depth = dungeon_get_next_level(player, player->max_depth, 1);
 	struct monster *t_mon = monster_target_monster(context);
 	struct loc decoy = cave_find_decoy(cave);
 
@@ -3453,11 +3454,11 @@ static bool effect_change_level(effect_handler_context_t *context, const char *r
 		up = false;
 
 	/* No forcing player down to quest levels if they can't leave */
-	if (!up && is_blocking_quest(target_depth))
+	if (!up && is_blocking_quest(player, target_depth))
 		down = false;
 
-	/* Can't leave quest levels or go down deeper than the dungeon */
-	if (is_blocking_quest(player->depth) || (player->depth >= z_info->max_depth - 1))
+	/* Can't leave blocking quest levels or go down deeper than the dungeon */
+	if (is_blocking_quest(player, player->depth) || (player->depth >= z_info->max_depth - 1))
 		down = false;
 
 	/* Determine up/down if not already done */
@@ -3471,16 +3472,19 @@ static bool effect_change_level(effect_handler_context_t *context, const char *r
 	/* Now actually do the level change */
 	if (up) {
 		msgt(MSG_TPLEVEL, rise ? rise : "You rise up through the ceiling.");
-		target_depth = dungeon_get_next_level(player->depth, -1);
+		target_depth = dungeon_get_next_level(player,
+			player->depth, -1);
 		dungeon_change_level(player, target_depth);
 	} else if (down) {
 		msgt(MSG_TPLEVEL, sink ? sink : "You sink through the floor.");
 
 		if (OPT(player, birth_force_descend)) {
-			target_depth = dungeon_get_next_level(player->max_depth, 1);
+			target_depth = dungeon_get_next_level(player,
+				player->max_depth, 1);
 			dungeon_change_level(player, target_depth);
 		} else {
-			target_depth = dungeon_get_next_level(player->depth, 1);
+			target_depth = dungeon_get_next_level(player,
+				player->depth, 1);
 			dungeon_change_level(player, target_depth);
 		}
 	} else {

src/trap.c
@@ -300,7 +300,7 @@ static int pick_trap(struct chunk *c, int feat, int trap_level)
 		/* Check legality of trapdoors. */
 		if (trf_has(kind->flags, TRF_DOWN)) {
 			/* No trap doors on quest levels */
-			if (is_active_quest(player->depth)) continue;
+			if (is_active_quest(player, player->depth)) continue;
 
 			/* No trap doors on the deepest level */
 			if (player->depth >= z_info->max_depth - 1)
@@ -558,7 +558,8 @@ extern void hit_trap(struct loc grid, int delayed)
 		/* Some traps drop you a dungeon level */
 		if (trf_has(trap->kind->flags, TRF_DOWN))
 			dungeon_change_level(player,
-								 dungeon_get_next_level(player->depth, 1));
+				dungeon_get_next_level(player,
+				player->depth, 1));
 
 		/* Some traps drop you onto them */
 		if (trf_has(trap->kind->flags, TRF_PIT))