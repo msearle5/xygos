e95744c506dd4a7b16d993130646ba78e6e24b25
Eric Branlund <ebranlund@fastmail.com>
2021-08-13 18:45:44 -0700

    Change player_attack_random_monster() to call py_attack() directly.  Resolves https://github.com/angband/angband/issues/5045 (part of https://github.com/angband/angband/issues/4934 ).  Does change the behavior:  camouflaged monsters will not be subject to the random attack.

--- src/player-util.c
+++ src/player-util.c
@@ -33,6 +33,7 @@
 #include "obj-pile.h"
 #include "obj-tval.h"
 #include "obj-util.h"
+#include "player-attack.h"
 #include "player-calcs.h"
 #include "player-history.h"
 #include "player-quest.h"
@@ -871,10 +872,11 @@ bool player_attack_random_monster(struct player *p)
 	/* Look for a monster, attack */
 	for (i = 0; i < 8; i++, dir++) {
 		struct loc grid = loc_sum(p->grid, ddgrid_ddd[dir % 8]);
-		if (square_monster(cave, grid)) {
+		const struct monster *mon = square_monster(cave, grid);
+		if (mon && !monster_is_camouflaged(mon)) {
 			p->upkeep->energy_use = z_info->move_energy;
 			msg("You angrily lash out at a nearby foe!");
-			move_player(ddd[dir % 8], false);
+			py_attack(p, grid);
 			return true;
 		}
 	}
