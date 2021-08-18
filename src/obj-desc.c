d0e35e0a8f8065834f7461e9f54bb896f391f296
Nick McConnell <nckmccnnll@gmail.com>
2021-08-19 09:38:46 +1000

    Fix bug in randart generation

--- src/obj-desc.c
+++ src/obj-desc.c
@@ -598,7 +598,7 @@ static size_t obj_desc_inscrip(const struct object *obj, char *buf,
 		u[n++] = "faulty";
 
 	/* Note ignore */
-	if (ignore_item_ok(p, obj))
+	if (p && ignore_item_ok(p, obj))
 		u[n++] = "ignore";
 
 	/* Note quest */
