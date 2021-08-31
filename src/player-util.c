angband/angband

Allow releasing of command spell under circumstances where 'r' would run afoul of the checks for whether reading is possible.  Resolves https://github.com/angband/angband/issues/5056 .

src/player-util.c
@@ -1103,7 +1103,12 @@ bool player_can_study_prereq(void)
  */
 bool player_can_read_prereq(void)
 {
-	return player_can_read(player, true);
+	/*
+	 * Accomodate hacks elsewhere:  'r' is overloaded to mean
+	 * release a commanded monster when TMD_COMMAND is active.
+	 */
+	return (player->timed[TMD_COMMAND]) ?
+		true : player_can_read(player, true);
 }
 
 /**

msearle5/xygos

src/player-util.c
@@ -1274,7 +1274,12 @@ bool player_can_cast_prereq(void)
  */
 bool player_can_run_prereq(void)
 {
-	return player_can_run(player, true);
+	/*
+	 * Accomodate hacks elsewhere:  'r' is overloaded to mean
+	 * release a commanded monster when TMD_COMMAND is active.
+	 */
+	return (player->timed[TMD_COMMAND]) ?
+		true : player_can_run(player, true);
 }
 
 /**