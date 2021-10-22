06e8d0b4531cf8d35a62f15c110e853b1f710465
Eric Branlund <ebranlund@fastmail.com>
2021-10-22 12:00:48 -0700

    In prepare_next_level(), copy known level name to separate buffer.  May resolve the report here, http://angband.oook.cz/forum/showthread.php?t=10942 , where exiting from Single Combat crashed because there were multiple known versions of the level in the chunk list.  That's also related to https://github.com/angband/angband/issues/4249 .

--- src/generate.c
+++ src/generate.c
@@ -1424,7 +1424,7 @@ void prepare_next_level(struct player *p)
 		if (old_level && (old_level != cave)) {
 			int i;
 			bool arena = cave->name && streq(cave->name, "arena");
-			char *known_name = format("%s known", name);
+			char *known_name = string_make(format("%s known", name));
 			struct chunk *old_known = chunk_find_name(known_name);
 			assert(old_known);
 
@@ -1501,6 +1501,7 @@ void prepare_next_level(struct player *p)
 			/* Remove from the list */
 			chunk_list_remove(name);
 			chunk_list_remove(known_name);
+			string_free(known_name);
 		} else if (p->upkeep->arena_level) {
 			/* We're creating a new arena level */
 			//p->upkeep->health_who[0] = health_who[0];
