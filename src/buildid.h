9f106a3ff3362fef566cd687b225b9bfe1030924
backwardsEric <55062581+backwardsEric@users.noreply.github.com>
2021-12-14 13:05:19 -0800

    Change how src/Makefile deals with the build ID to avoid requiring GNU Make (#5191)
    * Use buildid or buildver rather than explicit references to VERSION_STRING.  That way changes to the build number only require recompiling buildid.o and relinking.
    
    * Change how build ID is handled in src/Makefile to avoid requiring GNU Make.

--- src/buildid.h
+++ src/buildid.h
@@ -21,14 +21,6 @@
 
 #define VERSION_NAME	"Xygos"
 
-#ifdef BUILD_ID
-# define STR(x) #x
-# define XSTR(x) STR(x)
-# define VERSION_STRING XSTR(BUILD_ID)
-#else
-# define VERSION_STRING "0.1.1"
-#endif
-
 extern const char *buildid;
 extern const char *buildver;
 extern const char *copyright;
